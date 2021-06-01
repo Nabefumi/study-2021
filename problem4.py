a6e88df847f9540a51ca121b787495aa1ef81f84
Takafumi Watanabe <36560121+Nabefumi@users.noreply.github.com>
2021-05-31 21:15:54 -0700

    20200530-Python-problem1
    - Implement a function which receives a list of numbers as an input parameter. The
    function will find a sub-list from the list which is sorted and its length is greater than 4
    and return the sub-list. If there are more than 1 sub-list, the function can return only one
    of them. If there is no such sub-list in the list, the function will return None.

--- problem4.py
+++ problem4.py
@@ -1,30 +1,26 @@
 '''
-Suppose a list of numbers is given.Write a recursive 
-function which calculates the Even Factorof a given index of the list.
-
-Even Factor of a given index, i, is the number of numbers in the 
-list whose index is lower (smaller) than i.
-
-o Even Factor of index 3 is 1. Because there is only one even number (10) before index 3.
-o Even Factor of index 0 is 0. Because there is no even number in the list before index 0.
-o Even Factor of index 5 is 2 because there are 2 even numbers (10 and 10) before index 5. 
-
+- Implement a function which receives a list of numbers as an input parameter. The
+function will find a sub-list from the list which is sorted and its length is greater than 4
+and return the sub-list. If there are more than 1 sub-list, the function can return only one
+of them. If there is no such sub-list in the list, the function will return None. 
 '''
-#Problem4
 
-def evenFactor(nums, index, elementsChecked, total):
-    if len(nums) == 0:
-        return total
-    elif elementsChecked == index:
-        return total
-    else:
-        if nums[0] % 2 == 0:
-            total = total + 1
-        elementsChecked = elementsChecked + 1
-        return evenFactor(nums[1:], index, elementsChecked, total)
+def findSortedSublist(numbers):
+    
+    while True:
+        if len(numbers) >= 4:
+            for size in range(4, len(numbers)):
+                for startIndex in range(len(numbers)-size):
+                    subList = numbers[startIndex:startIndex+size]
+                    if subList == sorted(subList) or subList == sorted(subList, reverse = True):
+                        print("Sublist is found", subList)
+        
+            print("There is no such a subList")
+            return None
+        else:
+            print("There is no such a sublist")
+            return None
 
-#Initializing list
-nums=[10,11,15,13,10,1,10,16,20]
-index = int(input("Even Factor of index: "))
-#Calling recursive function
-print("is", evenFactor(nums, index, 0, 0))
+def main():
+    result = findSortedSublist([1,4,5,8,9,6,6,-1,2,6])
+main()
