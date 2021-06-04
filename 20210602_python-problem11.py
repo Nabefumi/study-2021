9ff0cf800629e4a01539d057245a491d50467500
Takafumi Watanabe <36560121+Nabefumi@users.noreply.github.com>
2021-06-03 20:00:24 -0700

    Update 20210602_python-problem11.py

--- 20210602_python-problem11.py
+++ 20210602_python-problem11.py
@@ -7,11 +7,14 @@ the number from the list and finally will return the new list.-Hint: Use a list
 '''
 
 def removeNumber(listOfNumbers, number):
- 
-    for i in listOfNumbers:
-        if(i == number): 
-            listOfNumbers.remove(i) 
-    return listOfNumbers 
     
-newList = removeNumber([1,2,3,4,5,6,7,8,9,0,1,2,3,1],1)
-print("After removing the occurrences of the number, the result is ",newList)
+    newList = []
+    
+    for i in listOfNumbers:
+        if i != number: 
+            newList.append(i) 
+    return newList 
+
+def main():    
+    print(removeNumber([1,2,3,4,5,6,7,8,9,0,1,2,3,1],1))
+main()
